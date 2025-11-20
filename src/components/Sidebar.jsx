833a8c86eafb82acbbbd18eea7c15414ee5a562e
sahanimanas <manshvisahani@gmail.com>
2025-11-20 22:18:02 +0530

    side bar update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ const [isOpen, setIsOpen] = React.useState(true);
 <Link to='/incidents' className="px-3 py-2 rounded-lg hover:bg-gray-800">Incidents</Link>
 <Link to='/analytics' className="px-3 py-2 rounded-lg hover:bg-gray-800">PPE Analytics</Link>
 <Link to='/playback' className="px-3 py-2 rounded-lg hover:bg-gray-800">Playback</Link>
-<Link to='/reports' className="px-3 py-2 rounded-lg hover:bg-gray-800">Reports</Link>
+{/* <Link to='/reports' className="px-3 py-2 rounded-lg hover:bg-gray-800">Reports</Link> */}
 <Link to='/settings' className="px-3 py-2 rounded-lg hover:bg-gray-800">Settings</Link>
 </nav>
 </aside>
