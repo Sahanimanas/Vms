5b40d5792dbe0d8ec6adc95a6b69f7517a0ced19
sahanimanas <manashvisahani@gmail.com>
2025-12-16 12:44:31 +0530

    setting page

--- src/Approutes.jsx
+++ src/Approutes.jsx
@@ -3,6 +3,7 @@ import DashboardPage from './pages/DashboardPage'
 import IncidentManagementPage from './pages/IncidentManagementPage'
 import AnalyticsReportsPage from './pages/AnalyticsReportsPage'
 import PlaybackPage from './pages/Playback'
+import Setting from './pages/Setting'
 const Approutes = () => {
   return (
     <Routes>
@@ -10,6 +11,9 @@ const Approutes = () => {
       <Route path='/incidents' element={<IncidentManagementPage/>}/>
       <Route path='/analytics' element={<AnalyticsReportsPage/>}/>
       <Route path='/playback' element={<PlaybackPage/>}/>
+      <Route path='/settings' element={<Setting/>}/>
+      <Route path='*' element={<div>404 Not Found</div>}/>
+
     </Routes>
   )
 }
---
+++ src/pages/Setting.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Setting = () => {
+  return (
+<div className='flex justify-center items-center h-screen bg-gray-800'>
+ <div className='mx-auto text-white text-3xl'>Setting Page</div>
+ </div>
+  )
+}
+
+export default Setting
